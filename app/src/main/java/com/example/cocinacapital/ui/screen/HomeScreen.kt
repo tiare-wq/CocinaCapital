7973003ed70c27138e9f20b0391ebd877a660e2a
Tiare <tiaresalazar.g@outlook.com>
2026-09-20 23:00:44 -0300

    feat: Diseñar logo de la aplicación

--- app/src/main/java/com/example/cocinacapital/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/cocinacapital/ui/screen/HomeScreen.kt
@@ -1,14 +1,25 @@
 package com.example.cocinacapital.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.cocinacapital.R
 
 @Composable
 fun HomeScreen(
@@ -19,7 +30,18 @@ fun HomeScreen(
             .fillMaxSize()
             .background(color = MaterialTheme.colorScheme.background)
     ) {
-        Row {
+
+        Row(
+            modifier = Modifier
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.hoja1),
+                contentDescription = "Logo de Cocina Capital",
+                modifier = Modifier
+                    .widthIn(max = 48.dp)
+            )
             Text(
                 text="Cocina",
                 style = MaterialTheme.typography.headlineMedium,
--- app/src/main/java/com/example/cocinacapital/ui/theme/Type.kt
+++ app/src/main/java/com/example/cocinacapital/ui/theme/Type.kt
@@ -17,14 +17,14 @@ val Typography = Typography(
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = firaSans.ttf.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = firaSans.ttf.Default,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
